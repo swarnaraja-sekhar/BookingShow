d51c79141f099c3be39456671c196cddfddbe5d9
swarnaraja-sekhar <bhuchiki12@gmail.com>
2025-07-09 19:56:39 +0530

    new

--- movie/src/components/MovieCard.jsx
+++ movie/src/components/MovieCard.jsx
@@ -25,7 +25,7 @@ const navigate = useNavigate()
         <div className='flex items-center justify-between mt-4 pd-3'>
             <button onClick={() => {navigate(`/movies/${movie._id}`);scrollTo(0,0)}}
              className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full
-            font-medium cursor-pointer bg-red-800'>Buy Tickets</button>
+            font-medium cursor-pointer '>Buy Tickets</button>
             
 
             <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
